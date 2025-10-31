335e20de323d92ff6c58ace9947ed5f111477405
annu32 <167179726+annu32@users.noreply.github.com>
2025-10-31 21:30:45 +0530

    live

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,7 +12,7 @@ function Login({ setPage, setUser }) {
 
     // 3. Add try...catch block for API call
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://fit-server-3.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
